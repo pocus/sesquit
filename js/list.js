574e89ec9cb9e836859a21a01d12395df322172e
David Yip <dvd.yip@gmail.com>
2014-04-01 21:48:35 -0400

    layout and animation

--- js/list.js
+++ js/list.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 
-	$(".check").click(function(){
 
-		$(this).html("<i class = 'fa fa-check-circle-o fa-4x target'></i>")
-		$('#myModal').foundation('reveal', 'open');
-		$("#cat-star").replaceWith("<div class='small-badge-image'>&nbsp</div><br>");
-		$("#m1").html("50 <i class='fa fa-book'></i>");
-		$("#m2").html("370 <i class='fa fa-files-o'></i>");
-	})
+$( ".highlight" ).hover(
+  function() {
+    $( ".ano-box" ).fadeIn(400);
+  }, function() {
+    $( ".ano-box" ).fadeOut(400);
+  }
+);
 
 })
